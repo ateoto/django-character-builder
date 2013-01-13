fdd3780e3afcd55f1226993b643a24aa5e0aeb3c
Matthew McCants <mattmccants@gmail.com>
2013-01-13 04:39:37 -0600

    Models getting closer.

--- character_builder/admin.py
+++ character_builder/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from character_builder.models import Source, Race, Role, ClassType, Character, Size
+from character_builder.models import Source, Race, Role, Skill, Ability, CharacterSkill, CharacterAbility, ClassType, Character, Size
 
 class ClassTypeAdmin(admin.ModelAdmin):
     list_display = ('name', 'role', 'source')
@@ -9,5 +9,9 @@ admin.site.register(Source)
 admin.site.register(Race)
 admin.site.register(Role)
 admin.site.register(Size)
+admin.site.register(Skill)
+admin.site.register(Ability)
+admin.site.register(CharacterSkill)
+admin.site.register(CharacterAbility)
 admin.site.register(ClassType, ClassTypeAdmin)
 admin.site.register(Character)
--- character_builder/models.py
+++ character_builder/models.py
@@ -12,8 +12,8 @@ class Source(models.Model):
 
 class Campaign(models.Model):
     name = models.CharField(max_length = 100)
-    dm = models.ForeignKey(User, related_name = "%(app_label)s_%(class)s_dm")
-    players = models.ManyToManyField(User, related_name = "%(app_label)s_$(class)s_players")
+    dm = models.ForeignKey(User, related_name = "+")
+    players = models.ManyToManyField(User, related_name = "+")
 
     def __unicode__(self):
         return self.name
@@ -38,6 +38,43 @@ class Vision(models.Model):
     def __unicode__(self):
         return self.name
 
+class Ability(models.Model):
+    name = models.CharField(max_length = 50)
+    abbreviation = models.CharField(max_length = 4)
+
+    class Meta:
+        verbose_name_plural = "Abilities"
+
+    def __unicode__(self):
+        return self.name
+
+class Skill(models.Model):
+    name = models.CharField(max_length = 50)
+    ability = models.ForeignKey(Ability)
+
+    def __unicode__(self):
+        return self.name
+
+class CharacterAbility(models.Model):
+    character = models.ForeignKey('Character')
+    ability = models.ForeignKey(Ability)
+    value = models.IntegerField()
+    
+    class Meta:
+        verbose_name_plural = "Character Abilities"
+
+    def __unicode__(self):
+        return "%s %s" % (self.ability.name, self.value)
+
+class CharacterSkill(models.Model):
+    character = models.ForeignKey('Character')
+    skill = models.ForeignKey(Skill)
+    is_trained = models.BooleanField(default = False)
+    value = models.IntegerField()
+
+    def __unicode__(self):
+        return "%s %s" % (self.skill.name, self.value)
+
 class Race(models.Model):
     name = models.CharField(max_length = 50)
     source = models.ForeignKey(Source)
@@ -46,12 +83,23 @@ class Race(models.Model):
     size = models.ForeignKey(Size)
     speed = models.IntegerField()
     vision = models.ForeignKey(Vision)
+    description = models.TextField()
 
     class Meta:
         ordering = ['name']
 
     def __unicode__(self):
         return self.name
+
+class RaceAbilityMod(models.Model):
+    race = models.ForeignKey(Race)
+    abilty = models.ForeignKey(Ability)
+    modifier = models.IntegerField()
+
+class RaceSkillMod(models.Model):
+    race = models.ForeignKey(Race)
+    skill = models.ForeignKey(Skill)
+    modifier = models.IntegerField()
 
 class ClassType(models.Model):
     name = models.CharField(max_length = 100)
@@ -65,20 +113,13 @@ class ClassType(models.Model):
         return self.name
 
 class Character(models.Model):
+    user = models.ForeignKey(User, related_name="+")
     name = models.CharField(max_length = 100)
     class_type = models.ForeignKey(ClassType)
     race = models.ForeignKey(Race)
     level = models.IntegerField()
-    
-    # Abilities
-    strength = models.IntegerField()
-    constitution = models.IntegerField()
-    dexterity = models.IntegerField()
-    intelligence = models.IntegerField()
-    wisdom = models.IntegerField()
-    charisma = models.IntegerField()
-
-    # Skills
+    xp = models.IntegerField()
+    hit_points = models.IntegerField()
 
     def __unicode__(self):
         return "%s Level %i %s %s" % (self.name, self.level, self.race.name, self.class_type.name)
