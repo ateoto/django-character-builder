2aa0bda97c283fe7fa18418c14523acf4b2a8c84
Matthew McCants <mattmccants@gmail.com>
2013-01-13 01:04:36 -0600

    Still working on the models.

--- character_builder/models.py
+++ character_builder/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Source(models.Model):
     name = models.CharField(max_length = 50)
@@ -11,6 +12,8 @@ class Source(models.Model):
 
 class Campaign(models.Model):
     name = models.CharField(max_length = 100)
+    dm = models.ForeignKey(User, related_name = "%(app_label)s_%(class)s_dm")
+    players = models.ManyToManyField(User, related_name = "%(app_label)s_$(class)s_players")
 
     def __unicode__(self):
         return self.name
@@ -29,12 +32,20 @@ class Size(models.Model):
     def __unicode__(self):
         return self.name
 
+class Vision(models.Model):
+    name = models.CharField(max_length = 50)
+
+    def __unicode__(self):
+        return self.name
+
 class Race(models.Model):
     name = models.CharField(max_length = 50)
     source = models.ForeignKey(Source)
     average_height_text = models.CharField(max_length = 50)
     average_weight_text = models.CharField(max_length = 50)
     size = models.ForeignKey(Size)
+    speed = models.IntegerField()
+    vision = models.ForeignKey(Vision)
 
     class Meta:
         ordering = ['name']
@@ -70,7 +81,7 @@ class Character(models.Model):
     # Skills
 
     def __unicode__(self):
-        return self.name
+        return "%s Level %i %s %s" % (self.name, self.level, self.race.name, self.class_type.name)
 
 class Item(models.Model):
     name = models.CharField(max_length = 100)
