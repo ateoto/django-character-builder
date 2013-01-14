a14e0951dbfb143ba1bdaf86b5202ddcb9c739a9
Matthew McCants <mattmccants@gmail.com>
2013-01-13 20:04:09 -0600

    Pylint and PEP8'ing some things.

--- character_builder/views.py
+++ character_builder/views.py
@@ -1,7 +1,47 @@
 from django.shortcuts import render_to_response
 from django.http import HttpResponse
+from django.template import Context, loader, RequestContext
 
-from character_builder.models import Character
+from character_builder.models import Character, Race, Source, ClassType
+from character_builder.forms import CharacterFormStepOne
+
 
 def index(request):
-    return render_to_response('character_builder/builder.html',{})
+    form = CharacterFormStepOne()
+
+    allowed_source = Source.objects.get(name="Player's Handbook")
+
+    races = Race.objects.filter(source=allowed_source)
+    classtypes = ClassType.objects.filter(source=allowed_source)
+
+    response_dict = {}
+
+    t = loader.get_template('character_builder/race_info.html')
+    for race in races:
+        c = Context({'race': race})
+        race.html = t.render(c)
+
+    t = loader.get_template('character_builder/classtype_info.html')
+    for classtype in classtypes:
+        c = Context({'classtype': classtype})
+        classtypes.html = t.render(c)
+
+    response_dict['races'] = races
+    response_dict['classtypes'] = classtypes
+    response_dict['form'] = form
+    return render_to_response('character_builder/builder.html',
+            response_dict,
+            context_instance=RequestContext(request))
+
+
+def step_one(request):
+    if request.method == 'POST':
+        form = CharacterFormStepOne(request.POST)
+        if form.is_valid():
+            return HttpResponse('Rock and Roll')
+    else:
+        form = CharacterFormStepOne()
+
+        return render_to_response('character_builder/builder.html',
+                {'form': form},
+                context_instance=RequestContext(request))
