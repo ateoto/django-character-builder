7eaa6cf9a6b1653b3ce4c9c3e1aea45457be1c25
Matthew McCants <mattmccants@gmail.com>
2013-01-14 17:43:19 -0600

    Implemented Race and Class information on form selection.

--- character_builder/static/js/main.js
+++ character_builder/static/js/main.js
@@ -1,5 +1,12 @@
 $(function() {
 	$('#id_race').change(function(){
-		console.log('Something was changed.');
-	});		
+		race_id = $('#id_race').val();
+		$('#info > *').hide();
+		$('#race-' + race_id).show();
+	});
+	$('#id_class_type').change(function(){
+		class_type_id = $('#id_class_type').val();
+		$('#info > *').hide();
+		$('#classtype-' + class_type_id).show();
+	});
 });
