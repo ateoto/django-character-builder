c0146b4ae2fe45a6b6075d415a0f49e30e965b18
Matthew McCants <mattmccants@gmail.com>
2013-01-15 17:57:08 -0600

    Starting to come together.

--- character_builder/static/js/main.js
+++ character_builder/static/js/main.js
@@ -1,12 +1,16 @@
 $(function() {
-	$('#id_race').change(function(){
+	$('#id_race').change(function() {
 		race_id = $('#id_race').val();
 		$('#info > *').hide();
 		$('#race-' + race_id).show();
 	});
-	$('#id_class_type').change(function(){
+	$('#id_class_type').change(function() {
 		class_type_id = $('#id_class_type').val();
 		$('#info > *').hide();
 		$('#classtype-' + class_type_id).show();
 	});
 });
+
+function ability_modifer(ability_value) {
+	return Math.floor((Math.abs(ability_value) - 10) / 2)
+}
