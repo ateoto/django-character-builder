98f0ce953463ca150b18d3b8e2fb6d573fb95a90
Matthew McCants <mattmccants@gmail.com>
2013-01-19 10:55:26 -0600

    Various fixes to betterjs branch.

--- character_builder/static/js/main.js
+++ character_builder/static/js/main.js
@@ -1,6 +1,8 @@
 $(function() {
 	$('#info > *').hide();
+
 	$('.std-arr-ability').popover(options = {trigger: 'hover'}	);
+
 	$('#standard-array-abilities').sortable({
 		start: function(event, ui) {
 			$('.std-arr-ability').popover('hide');
@@ -13,24 +15,27 @@ $(function() {
 			}
 		}
 	});
-	$('#standard-array-abilities').disableSelection();
 
+	$('#standard-array-abilities').disableSelection();
 
 	$('#id_race').change(function() {
 		race_id = $('#id_race').val();
 		$('#info > *').hide();
 		$('#race-' + race_id).show();
 	});
+
 	$('#id_class_type').change(function() {
 		class_type_id = $('#id_class_type').val();
 		$('#info > *').hide();
 		$('#classtype-' + class_type_id).show();
 	});
+
 	$('#btn-personal').click(function() {
 		$('#info > *').hide();
 		$('#builder-ux > *').hide();
 		$('#personal').show();
 	});
+
 	$('#btn-abilities').click(function() {
 		$('#info > *').hide();
 		$('#builder-ux > *').hide();
@@ -38,11 +43,13 @@ $(function() {
 		$('#ability-info').show();
 
 	});
+
 	$('#btn-skills').click(function() {
 		$('#info > *').hide();
 		$('#builder-ux > *').hide();
 		$('#skills').show();
 	});
+
 	$('#btn-powers').click(function() {
 		$('#info > *').hide();
 		$('#builder-ux > *').hide();
