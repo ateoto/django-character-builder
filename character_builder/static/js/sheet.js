e794ac73212b7abd557569ae7a2cac225b92fd63
Matthew McCants <mattmccants@gmail.com>
2013-01-22 02:42:26 -0600

    Use the new pretty_mod function.

--- character_builder/static/js/sheet.js
+++ character_builder/static/js/sheet.js
@@ -42,17 +42,9 @@ function update_ability_scores(character_data) {
 		var element_id = $(this).attr('id');
 		var value = parseInt($('.ability-value', this).text(), 10);
 		var modifier = CharacterBuilder.ability_modifer(value);
-		if (modifier < 0)
-		{
-			prepend = '-';
-		} else if (modifier === 0) {
-			prepend = '';
-		} else {
-			prepend = '+';
-		}
-		$('.ability-mod', this).text(prepend + modifier);
+		$('.ability-mod', this).text(CharacterBuilder.pretty_mod(modifier));
 
 		// This is how we should actually work:
-		$('.ability-mod-level', this).text(modifier + CharacterBuilder.half_level(character_data.level))
+		$('.ability-mod-level', this).text(CharacterBuilder.pretty_mod(modifier + CharacterBuilder.half_level(character_data.level)));
 	});
 }
