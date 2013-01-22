680e719ba230998cbb207cb115d9716324a111c2
Matthew McCants <mattmccants@gmail.com>
2013-01-22 02:42:03 -0600

    pretty_mod function.

--- character_builder/static/js/CharacterBuilder.js
+++ character_builder/static/js/CharacterBuilder.js
@@ -15,6 +15,21 @@ var CharacterBuilder = {
 	standard_array : [16, 14, 13, 12, 11, 10],
 };
 
+CharacterBuilder.half_level = function(level) {
+	return Math.floor(level / 2);
+}
+
 CharacterBuilder.ability_modifer = function(ability_value) {
 	return Math.floor((Math.abs(ability_value) - 10) / 2);
+}
+
+CharacterBuilder.pretty_mod = function(modifier) {
+	if (modifier < 0) {
+		prepend = '-';
+	} else if (modifier > 0) {
+		prepend = '+';
+	} else {
+		prepend = '';
+	}
+	return prepend + modifier.toString();
 }
