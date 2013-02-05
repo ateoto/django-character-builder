10bd6daec68db2e0ffeb308d3315c8f92df7562e
Matthew McCants <mattmccants@gmail.com>
2013-02-05 14:11:58 -0600

    I blame my sprained fingers.

--- character_builder/api/api.py
+++ character_builder/api/api.py
@@ -5,8 +5,8 @@ from resources import (ClassTypeResource, RaceResource, GenderResource,
 
 v1 = Api(api_name='v1')
 
-v1.register(CharacterResource)
-v1.register(ClassTypeResource)
-v1.register(RaceResource)
-v1.register(DeityResource)
-v1.register(AlignmentResource)
+v1.register(CharacterResource())
+v1.register(ClassTypeResource())
+v1.register(RaceResource())
+v1.register(DeityResource())
+v1.register(AlignmentResource())
